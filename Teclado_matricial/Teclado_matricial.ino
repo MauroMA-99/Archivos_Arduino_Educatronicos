393e0cb31c0856f2f5b6de4e765c8427e29c7ef2
MauroMA-99 <mauro2017pre@gmail.com>
2022-05-20 09:53:52 -0500

    Se agrego este archivo

---
+++ Teclado_matricial/Teclado_matricial.ino
@@ -0,0 +1,28 @@
+#include <Keypad.h>
+
+const byte rows = 4;
+const byte cols = 4;
+char keys[rows][cols] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+byte rowPins[rows] = {2,3,4,5};
+byte colPins[cols] = {6,7,8,9};
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  char key = keypad.getKey();
+  if(key != NO_KEY)
+  {
+    Serial.println(key);
+  }
+}
